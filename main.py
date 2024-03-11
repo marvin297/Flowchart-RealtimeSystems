9ed85f7a1f06fcbe08aac782790a5baba2036cd9
Marlon O <skaaaatergaming@gmail.com>
2024-03-11 18:33:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,10 @@ class CustomCanvas(Canvas):
 
 
 class TaskConnector:
-    def __init__(self, canvas, x, y, x2, y2):
+    def __init__(self, canvas):
         self.canvas = canvas
-        self.line = canvas.create_line(x, y, x2, y2, width=5, fill="#ff335c")
+        self.circle_radius = 25
+        self.line = canvas.create_line(0, 0, 0, 0, width=5, fill="#ff335c", arrow="last")
         self.canvas.pack()
 
     def update_start(self, new_x, new_y):
@@ -45,15 +46,12 @@ class TaskConnector:
         self.canvas.coords(self.line, x1, y1, new_x, new_y)
 
 
+
 class DraggableTask:
-    def __init__(self, canvas, x, y, radius, root, connectors=None):
+    def __init__(self, canvas, x, y, radius, root):
         self.canvas = canvas
         self.oval = canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=root['bg'], outline="#ff335c", width=5)
-
-        self.connections = {
-            TaskConnector(self.canvas, 0, 0, 50, 50): "start",
-            TaskConnector(self.canvas, 100, 0, 50, 50): "end"
-        }
+        self.connectors = {}
 
         #self.canvas.tag_bind(self.oval, "<Button-1>", self.on_drag_start)
         self.canvas.tag_bind(self.oval, "<B1-Motion>", lambda event: self.on_drag(event))
@@ -69,15 +67,18 @@ class DraggableTask:
         self.update_connections()
 
     def update_connections(self):
-        for connection in self.connections:
+        for connection in self.connectors:
             self.canvas.tag_raise(self.oval, connection.line)
-            if self.connections[connection] == "start":
+            if self.connectors[connection] == "start":
                 connection.update_start(self.get_position()[0] + 50, self.get_position()[1] + 50)
-            elif self.connections[connection] == "end":
+            elif self.connectors[connection] == "end":
                 connection.update_end(self.get_position()[0] + 50, self.get_position()[1] + 50)
 
     def get_position(self):
         return self.canvas.coords(self.oval)
+
+    def set_connectors(self, connectors):
+        self.connectors = connectors
 
 
 class App(customtkinter.CTk):
@@ -95,7 +96,22 @@ class App(customtkinter.CTk):
         c = Canvas(self, bd=0, highlightthickness=0, background=self['bg'])
         c.pack(fill=BOTH, expand=True)
 
-        DraggableTask(c, 50, 50, 50, self)
+        task1 = DraggableTask(c, 50, 50, 50, self)
+        task2 = DraggableTask(c, 100, 100, 50, self)
+
+        tc1 = TaskConnector(c)
+        tc2 = TaskConnector(c)
+
+        task1.set_connectors({
+            tc1: "start",
+            tc2: "end"
+        })
+
+        task2.set_connectors({
+            tc2: "start"
+        })
+
+        task1.update_connections()
 
 
 if __name__ == "__main__":
